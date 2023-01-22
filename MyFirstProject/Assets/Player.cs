f5a37eb850183d5333677ae63b98ea49525f7f50
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-01-21 20:50:32 -0700

    Added and changed movement speeds

--- MyFirstProject/Assets/Player.cs
+++ MyFirstProject/Assets/Player.cs
@@ -13,14 +13,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * 0.5f;
+            transform.position += Vector3.forward * 0.25f;
         }
 
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * 0.5f;
+            transform.position += Vector3.back * 0.25f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.25f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.25f;
         }
     }
 }
